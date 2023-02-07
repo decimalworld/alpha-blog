c91b562c6f4b492364152658eade9f20f1686f2e
quanle <leminhquan8101998@gmail.com>
2023-02-07 10:17:28 +0700

    add edit and update action for user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,13 +3,27 @@ class UsersController < ApplicationController
         @user = User.new
     end
 
+    def edit
+        @user = User.find(params[:id])
+    end
+
     def create
         @user = User.new(user_params)
         if @user.save
             flash[:notice] = "Welcome to the Alpha Blog #{@user.username}, you have successfully sign up"
             redirect_to articles_path
         else
-            render 'new', status: :unprocessable_entity
+            render :new, status: :unprocessable_entity
+        end
+    end
+
+    def update
+        @user = User.find(params[:id])
+        if @user.update(user_params)
+            flash[:notice] = "Your account information has been successfully updated"
+            redirect_to articles_path
+        else
+            render :edit, status: :unprocessable_entity
         end
     end
 
