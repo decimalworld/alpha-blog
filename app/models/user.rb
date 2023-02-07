691b6098d5979a093e0ef30220239f54a785a075
quanle <leminhquan8101998@gmail.com>
2023-02-07 09:28:25 +0700

    Add password digest to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,7 @@ class User < ApplicationRecord
                     uniqueness: { case_sensitivity: false }, 
                     length: {maximum: 105},
                     format: { with: VALID_EMAIL_REGEX }
+    has_secure_password
 
     private
     def downcaseEmail
