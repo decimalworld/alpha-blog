6414966a9251f7ec8e031715485d0c599551bb19
quanle <leminhquan8101998@gmail.com>
2023-02-09 22:13:40 +0700

    Add edit categories

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,4 +1,5 @@
 class CategoriesController < ApplicationController
+    before_action :set_category, only: [:show, :edit, :update]
     before_action :require_admin, except: [:index, :show]
 
     def index
@@ -6,7 +7,6 @@ class CategoriesController < ApplicationController
     end
     
     def show
-        @category = Category.find(params[:id])
         @articles = @category.articles.paginate(page: params[:page],per_page:2)
     end
 
@@ -27,7 +27,20 @@ class CategoriesController < ApplicationController
         end
     end
 
+    def update
+        if @category.update(category_params)
+            flash[:notice] = "Category was successfully updated"
+            redirect_to @category
+        else
+            render :new, status: :unprocessable_entity
+        end
+    end
+
     private
+    def set_category
+        @category = Category.find(params[:id])
+    end
+
     def category_params
         params.require(:category).permit(:name)
     end
