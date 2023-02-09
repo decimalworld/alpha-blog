0558c0d3575ae03c1c3fcc59e9b3a4869481b28a
quanle <leminhquan8101998@gmail.com>
2023-02-09 21:49:52 +0700

    update article views

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,7 @@
 class Article < ApplicationRecord
     belongs_to :user
+    has_many :article_category
+    has_many :categories, through: :article_category
     validates :title, presence: true
     validates :title, length: {minimum: 6, maximum: 100}
     validates :description, presence: true
